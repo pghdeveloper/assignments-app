2ef7b826deacc554ab73adbdc7b730d4e41686da
pghdeveloper <34989427+pghdeveloper@users.noreply.github.com>
2023-07-23 23:34:50 -0400

    First Push

---
+++ src/CreateAssignmentPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateAssignmentForm from './components/CreateAssignmentForm';
+
+const CreateAssignmentPage = () => {
+  return (
+    <div>
+      <h2>Create New Assignment</h2>
+      <CreateAssignmentForm />
+    </div>
+  );
+};
+
+export default CreateAssignmentPage;
